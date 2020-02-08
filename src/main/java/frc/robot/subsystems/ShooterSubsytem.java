e5aa1a067e54b2d2e5b8cc225ed576c96d888e29
Jade Kessinger <jadekessinger13@gmail.com>
2020-02-08 15:25:12 -0800

    Removed a few unused imports.

--- src/main/java/frc/robot/subsystems/ShooterSubsytem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsytem.java
@@ -10,7 +10,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
